4c26569f9995e2186c4e71ce8c213bc1b0925ec0
abruzzisz <abruzzijulia69@gmail.com>
2025-10-13 09:21:49 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,8 +1,51 @@
-function executar() {
-    console.log(perguntar("É mamifero?"));
+async function executar() {
+    if (await perguntar("É mamifero?")) {
+        if (await perguntar("É quadrupede?")) {
+            if (await perguntar("É carnivoro? ")) {
+                resposta("É o LEÃO");
+            } else if (await perguntar("É herbivoro?")) {
+                resposta("É o CAVALO");
+            } else (resposta(""))
+        }
+        else if (await perguntar("É bípede?")) {
+            if (await perguntar("É onívoro?")) {
+                resposta("É o HOMEM");
+            } else if (await perguntar("É frutífero?")) {
+                resposta("É o MACACO");
+            } else (resposta(""))
+        }
+        else if (await perguntar("É voador?")) {
+            resposta("É o MORCEGO");
+        } 
+        else if (await perguntar("É aquático?")) {
+            resposta("É a BALEIA");
+        } else (resposta(""))
+    } else if (await perguntar("É ave?")) {
+        if (await perguntar("Não voador?")) {
+            if (await perguntar("É Tropical ?")) {
+                resposta("Avestruz");
+            } else if (await perguntar("É Polar?")) {
+                resposta("É o PINGUIM");
+            } else (resposta(""))
+        }
+        else if (await perguntar("É aquático")) {
+            resposta("É o PATO");
+        }
+        else if (await perguntar("É de rapina?")) {
+            resposta("É a ÁGUIA");
+        } else (resposta(""))
+    } else if (await perguntar("É réptil?")) {
+        if (await perguntar("Tem casco?")) {
+            resposta("É a TARTARUGA");
+        } else if (await perguntar("É carnívoro")) {
+            resposta("É o CROCODILO");
+        } else (resposta(""))
+    } else (resposta(""))
 }
-function perguntar(texto) {
-    Swal.fire({
+
+
+async function perguntar(texto) {
+    const result = await Swal.fire({
         title: texto,
         icon: "question",
         showCancelButton: true,
@@ -10,7 +53,20 @@ function perguntar(texto) {
         cancelButtonColor: "#d33",
         confirmButtonText: "Sim",
         cancelButtonText: "Não"
-    }).then((result) => {
-        return  result.isConfirmed;
     });
+    return result.isConfirmed;
+}
+
+function resposta(texto) {
+    if (texto !== "") {
+        Swal.fire({
+            title: texto,
+            icon: "success"
+        });
+    } else {
+        Swal.fire({
+            title: "Animal não encontrado!",
+            icon: "error"
+        });
+    }
 }
